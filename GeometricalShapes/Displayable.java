d41c6f4570fd191ea794dd9a81dc56113d17e4a9
hhouda <houdahdili2003@gmail.com>
2025-09-27 18:05:03 +0100

    finished triangle and rectangle

--- GeometricalShapes/Displayable.java
+++ GeometricalShapes/Displayable.java
@@ -3,7 +3,6 @@ package GeometricalShapes;
 import java.awt.Color;
 
 public interface Displayable {
-    
     void display(int x, int y, Color color);
     void save(String string);
 }
--- GeometricalShapes/Shapes/Rectangle.java
+++ GeometricalShapes/Shapes/Rectangle.java
@@ -1,5 +1,53 @@
 package GeometricalShapes.Shapes;
 
-public class Rectangle {
-    
+import GeometricalShapes.*;
+import java.awt.Color;
+import java.util.Random;
+
+public class Rectangle implements Drawable {
+    private Point topLeft;
+    private Point bottomRight;
+    private Color color;
+
+    public Rectangle(Point topLeft, Point bottomRight) {
+        this.topLeft = topLeft;
+        this.bottomRight = bottomRight;
+        this.color = randomColor();
+    }
+
+    private Color randomColor() {
+        return Color.WHITE;
+    }
+
+    public Point getTopLeft() {
+        return topLeft;
+    }
+
+    public Point getBottomRight() {
+        return bottomRight;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void draw(Displayable displayable) {
+        int x1 = topLeft.getX();
+        int y1 = topLeft.getY();
+        int x2 = bottomRight.getX();
+        int y2 = bottomRight.getY();
+
+        Point topRight = new Point(x2, y1);
+        Point bottomLeft = new Point(x1, y2);
+
+        Line top = new Line(topLeft, topRight);
+        Line right = new Line(topRight, bottomRight);
+        Line bottom = new Line(bottomRight, bottomLeft);
+        Line left = new Line(topLeft, bottomLeft);
+
+        top.draw(displayable);
+        right.draw(displayable);
+        bottom.draw(displayable);
+        left.draw(displayable);
+    }
 }
